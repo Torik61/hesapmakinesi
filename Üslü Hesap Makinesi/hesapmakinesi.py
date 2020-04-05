c85c22233fb16847e16199d74f9daee383bc630f
Torik61 <50461548+Torik61@users.noreply.github.com>
2020-04-05 16:01:00 +0300

    Add files via upload

---
+++ Üslü Hesap Makinesi/hesapmakinesi.py
@@ -0,0 +1,29 @@
+z = 0
+
+print("Üslü sayı hesap makinesine hoşgeldiniz.")
+
+while z < 3 :
+
+    x = input("Taban: ")
+    y = input("Üs: ")
+        
+        #print(type(x))
+        #print(type(y))
+
+
+    try:
+        sonuc = (float(x) ** float(y))
+        print("Sonuc: " + str(sonuc))
+        z = 5
+    except:
+        print("Lütfen sayı giriniz.")
+        z = z + 1 
+
+    if int(z) == 3:
+        print("3 Kere hata yaptınız. Program kapatılıyor.")
+    
+        
+
+
+
+
